149e0fbb5b379118a320f8bc9ac17e558d35ff8a
tasmia2016831022 <42036165+tasmia2016831022@users.noreply.github.com>
2018-10-15 16:37:35 +0600

    Add files via upload

---
+++ 1293_Eniya.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int n, a, b , s;
+
+    scanf("%d%d%d",&n,&a,&b);
+
+    s=(a*b)*n*2;
+
+    printf("%d\n",s);
+
+    return 0;
+
+
+
+}
+
